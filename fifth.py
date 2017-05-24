beb337c480c2592d991e5103fa69714f6f109854
Zetos Lima <cesar_manoel@hotmail.com>
2017-05-23 22:51:03 -0300

    Tuples and Lists

--- fifth.py
+++ fifth.py
@@ -6,3 +6,11 @@ def quotient_and_remainder(x,y):
 (quot, rem) = quotient_and_remainder(4,5)
 
 print((quot, rem))
+
+# Tuples = () immutable
+# Lists = [] mutable
+
+# Clone a List:
+
+satan = ['666',7,'bah']
+hell = satan[:] #hell is a cloned list of satan | satan[0:len(satan)]

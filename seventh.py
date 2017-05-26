a0456672d0fe4305288c2e898ab161cfd0159ca8
Zetos Lima <cesar_manoel@hotmail.com>
2017-05-25 23:09:34 -0300

    Assertion comment

--- seventh.py
+++ seventh.py
@@ -14,6 +14,7 @@ def get_ratios(L1, L2):
             raise ValueError('get_ratios called with a bad arg')
     return ratios
 
+# Assertions
 
 def avg(grades):
     assert not len(grades) == 0, 'no grades data'
